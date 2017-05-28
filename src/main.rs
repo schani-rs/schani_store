69cc0a234fd7629fee08c051e5718a4a0b7e6f6c
stefan <stefan.roesch1985@gmail.com>
2017-05-28 11:51:21 +0200

    add tag update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 extern crate schani_store;
 extern crate diesel;
 
+use self::schani_store::services::tag_service;
 
-fn main() {}
+fn main() {
+    let new_tag = tag_service::create("Marco").expect("Could not create");
+    println!("NewTag: {} Label: {}", new_tag.id, new_tag.label);
+
+    let mut tag = tag_service::find(new_tag.id).expect("Not found");
+    println!("Tag: {} Label: {}", tag.id, tag.label);
+
+    tag.label = "Polo!".to_string();
+    tag = tag_service::update(&tag).expect("Could not update");
+    println!("UpdatedTag: {} Label: {}", tag.id, tag.label);
+}
--- src/services/tag_service.rs
+++ src/services/tag_service.rs
@@ -9,17 +9,22 @@ use super::super::db_manager;
 pub fn create<'a>(label: &'a str) -> Result<Tag, Box<Error>> {
     let new_tag = NewTag { label: label };
     let ref conn = *try!(db_manager::POOL.get());
-    let result = diesel::insert(&new_tag).into(tags::table).get_result(conn);
-    result
-        .ok()
-        .ok_or(From::from(format!("Could not create tag with label: {}", label)))
+    let result = try!(diesel::insert(&new_tag).into(tags::table).get_result(conn));
+    Ok(result)
 }
 
 pub fn find(pk: i32) -> Result<Tag, Box<Error>> {
     use schema::tags::dsl::*;
-    let ref conn = *db_manager::POOL.get().expect("Bad Connection");
-    let mut result = try!(tags.find(pk).load::<Tag>(conn));
-    result
-        .pop()
-        .ok_or(From::from(format!("Tag with id: {} not found", pk)))
+    let ref conn = *try!(db_manager::POOL.get());
+    let result = try!(tags.find(pk).first(conn));
+    Ok(result)
+}
+
+pub fn update(tag: &Tag) -> Result<Tag, Box<Error>> {
+    use schema::tags::dsl::*;
+    let ref conn = *try!(db_manager::POOL.get());
+    let result = try!(diesel::update(tags.find(tag.id))
+                          .set(label.eq(tag.label.clone()))
+                          .get_result(conn));
+    Ok(result)
 }
