63f1e1cbfe352ef4d817928a4167ba7c21207617
stefan <stefan.roesch1985@gmail.com>
2017-06-06 19:18:33 +0200

    Add file storage to raw image create function

--- src/services/raw_image_service.rs
+++ src/services/raw_image_service.rs
@@ -6,10 +6,15 @@ use schema::raw_images::table;
 use schema::raw_images::dsl::*;
 use super::super::models::{RawImage, NewRawImage, Image};
 use super::super::db_manager;
+use super::file_storage;
 
-pub fn create(new_raw_image: &NewRawImage) -> Result<RawImage, Box<Error>> {
+pub fn create(new_raw_image: &NewRawImage, data: &[u8]) -> Result<RawImage, Box<Error>> {
     let ref conn = *try!(db_manager::POOL.get());
-    let result = try!(diesel::insert(new_raw_image).into(table).get_result(conn));
+    let result = try!(conn.transaction::<RawImage, Box<Error>, _>(|| {
+        let result: RawImage = try!(diesel::insert(new_raw_image).into(table).get_result(conn));
+        try!(file_storage::store_raw_image(result.id, data));
+        Ok(result)
+    }));
     Ok(result)
 }
 
@@ -41,4 +46,33 @@ pub fn get_images_of_raw_image(raw_image: &RawImage) -> Result<Vec<Image>, Box<E
     let ref conn = *try!(db_manager::POOL.get());
     let result = try!(Image::belonging_to(raw_image).load(conn));
     Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use diesel::pg::data_types::PgDate;
+
+    #[test]
+    fn test_create() {
+        let image = b"Hello, wurst!";
+        let raw_img = NewRawImage {
+            user_id: 999,
+            camera: "megapixelzoom",
+            latitude: 0.22,
+            longitude: 0.32,
+            creation: PgDate(0),
+        };
+        if let Err(x) = create(&raw_img, image) {
+            println!("err: {}", x);
+            panic!();
+        }
+        // match load_image(1) {
+        //     Ok(i) => assert_eq!(image, i.as_slice()),
+        //     Err(x) => {
+        //         println!("err: {}", x);
+        //         panic!();
+        //     }
+        // }
+    }
 }
