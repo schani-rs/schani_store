0d5967bc5a99b639d2906e7b78fe452d28a0b9d8
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-12-11 14:21:57 +0100

    Read storage host from env var
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate dotenv;
 extern crate schani_store;
 
+use std::env;
+
 fn main() {
     if dotenv::dotenv().is_err() {
         println!("No .env found, relying solely on env vars.");
     }
 
-    let app = schani_store::StoreWebApp::new("http://127.0.0.1:9100".parse().unwrap());
+    let storage_url = env::var("STORAGE_HOST").expect("STORAGE_HOST env var not set");
+    let app = schani_store::StoreWebApp::new(storage_url.parse().unwrap());
     app.run();
 }
