a7bfd1377aad2553b0b39cd92eb7fa5315f6cbf4
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-12-11 13:26:04 +0100

    Fix panic if .env is missing
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ extern crate dotenv;
 extern crate schani_store;
 
 fn main() {
-    dotenv::dotenv().unwrap();
+    if dotenv::dotenv().is_err() {
+        println!("No .env found, relying solely on env vars.");
+    }
 
     let app = schani_store::StoreWebApp::new("http://127.0.0.1:9100".parse().unwrap());
     app.run();
